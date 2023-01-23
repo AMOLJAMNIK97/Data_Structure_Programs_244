91865fef3a445b8d74119b6c0f993b30912f2535
Amol <ajamnik.mech@gmail.com>
2023-01-23 22:42:48 +0530

    [Amol]- List of Numbers from a file and arrange it ascending Order

---
+++ DataStructurePrograms/DataStructurePrograms/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderedList orderedList = new OrderedList();
+
+            Console.WriteLine("Enter word to Search");
+            string word = Console.ReadLine();
+            orderedList.Ordered(word); 
+        }
+    }
+}
